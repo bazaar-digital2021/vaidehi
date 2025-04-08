92a1c1d460bb14795a6207f2cc2b46e04826df90
deadeye03 <yakshitkr@gmail.com>
2025-04-08 17:47:43 +0530

    footer fix and pages are live

--- src/lib/courses.ts
+++ src/lib/courses.ts
@@ -10,21 +10,21 @@ export const courses: CourseProps[] = [
     {
         title: "General Nursing & Midwifery (GNM)",
         image: "/images/gnm-students.png",
-        intake: 100,
+        intake: 60,
         duration: "3 Years",
         href: "/courses/gnm/course-description"
     },
     {
         title: "Basic B.Sc. Nursing",
         image: "/images/bsc-nursing-students.png",
-        intake: 100,
+        intake: 50,
         duration: "4 Years",
         href: "/courses/basic-bsc-nursing/course-description"
     },
     {
         title: "Auxiliary Nursing Midwifery (ANM)",
         image: "/images/post-certificate-students.png",
-        intake: 40,
+        intake: 60,
         duration: "2 Years",
         href: "/courses/anm/course-description"
     },
